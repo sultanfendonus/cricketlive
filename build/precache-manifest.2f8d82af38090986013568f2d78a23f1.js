23670b512ae881cdd6a6fcb426f1142533004f16
Sunny Sultan <sunnysultan1640@gmail.com>
2019-06-02 14:32:47 +0600

    build promlem solved by adding manually

---
+++ build/precache-manifest.2f8d82af38090986013568f2d78a23f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959ab52a8d5d77ca5d033cbfc542878a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ebe9914f8592533d8ac",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "bc04c339efc22777c9f1",
+    "url": "/static/css/main.35237b70.chunk.css"
+  },
+  {
+    "revision": "5ebe9914f8592533d8ac",
+    "url": "/static/js/2.4e1e5246.chunk.js"
+  },
+  {
+    "revision": "bc04c339efc22777c9f1",
+    "url": "/static/js/main.eddd0d2f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
